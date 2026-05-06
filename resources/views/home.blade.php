f6fe7be12503c0a552be9a1c3a2a159e9bb00f52
Laleilish <nfebri622@gmail.com>
2026-05-06 16:48:38 +0700

    feat: add AppLayout component, update home view to use component, and enhance app.css with utility classes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
-@extends('layouts.app')
+<x-app-layout>
 
-@section('content')
-    {{-- nanti diisi --}}
-@endsection
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-cream-bg overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{-- konten home kamu di sini --}}
+                    <h1>Welcome to Home Page!</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
