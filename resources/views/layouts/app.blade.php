6a8ac9a527513735200579dbf9d935fd8a13ec2f
Laleilish <nfebri622@gmail.com>
2026-05-27 00:35:15 +0700

    -

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-cream-bg">
-            {{-- Include Navbar (pakai navbar kustom, bukan navigation.blade.php) --}}
+            {{-- Include Navbar  --}}
             @include('components.navbar')
 
     <main>
