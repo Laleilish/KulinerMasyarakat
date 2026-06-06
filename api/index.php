93b6e7b256fba06dfdf66cc0b46613262143eb6d
Laleilish <nfebri622@gmail.com>
2026-06-07 01:40:48 +0700

    chore: add entry point script, enforce PHP 8.3 platform requirement, and update Guzzle dependencies

--- api/index.php
+++ api/index.php
@@ -5,12 +5,4 @@ error_reporting(E_ALL);
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 putenv('VIEW_COMPILED_PATH=/tmp');
 
-try {
-    require __DIR__ . '/../public/index.php';
-} catch (\Throwable $e) {
-    echo "<h1>🚨 Error Tertangkap Manual! 🚨</h1>";
-    echo "<b>Pesan Error:</b> " . $e->getMessage() . "<br><br>";
-    echo "<b>File:</b> " . $e->getFile() . "<br>";
-    echo "<b>Baris:</b> " . $e->getLine() . "<br>";
-    exit;
-}
+require __DIR__ . '/../public/index.php';
