1c9d490468b6712ecd362e26d9cc2c677871a596
Laleilish <nfebri622@gmail.com>
2026-06-07 01:25:40 +0700

    chore: enable debug mode and add entry point for PHP application configuration

--- api/index.php
+++ api/index.php
@@ -1,2 +1,8 @@
 <?php
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 require __DIR__ . '/../public/index.php';
